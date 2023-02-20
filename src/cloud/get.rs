ca35e53215ce456fda07ec0675cadd8b5cae571b
SeitaHigashi <higashi110902@gmail.com>
2023-02-21 00:57:39 +0900

    feat: update the home enum related

--- src/cloud/get.rs
+++ src/cloud/get.rs
@@ -1,14 +1,13 @@
-mod home;
+pub mod home;
 
+use home::*;
 use super::api::API;
 
 pub enum GET {
     Appliances,
     Signals{applianceid: String},
     Devices,
-    Homes,
-    HomeDevices{homeid: String},
-    HomeUsers{homeid: String},
+    Homes{home: Option<Home>},
     Invites{invitetoken: String},
     Users,
 }
@@ -19,9 +18,8 @@ impl API for GET {
             GET::Appliances => String::from("/1/appliances"),
             GET::Signals{ applianceid } => format!("/1/appliances/{}/signals/", applianceid),
             GET::Devices => String::from("/1/devices"),
-            GET::Homes => String::from("/1/homes"),
-            GET::HomeDevices { homeid } => format!("/1/homes/{}/devices", homeid),
-            GET::HomeUsers{ homeid } => format!("/1/homes/{}/users", homeid),
+            GET::Homes{ home: Option::None }=> String::from("/1/homes"),
+            GET::Homes{ home: Option::Some(home) }=> format!("/1/homes{}", home.path()),
             GET::Invites{ invitetoken } => format!("/1/invites/{}", invitetoken),
             GET::Users => String::from("/1/users/me"),
         }
--- src/cloud/get/home.rs
+++ src/cloud/get/home.rs
@@ -1,3 +1,16 @@
 pub enum Home {
+    None{homeid: Option<String>},
+    Devices{homeid: String},
+    Users{homeid: String},
+}
 
+impl Home {
+    pub fn path(&self) -> String {
+        match self {
+            Home::None { homeid: Option::None } => String::from(""),
+            Home::None { homeid: Some(homeid) } => format!("/{}", homeid),
+            Home::Devices { homeid } => format!("/{}/devices", homeid),
+            Home::Users { homeid } => format!("/{}/users", homeid),
+        }
+    }
 }
