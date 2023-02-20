d5b3a1bb7b8fecb5d915502a83ca290e4605a660
SeitaHigashi <higashi110902@gmail.com>
2023-02-21 00:16:05 +0900

    fix: update get enum

--- src/cloud/get.rs
+++ src/cloud/get.rs
@@ -6,7 +6,7 @@ pub enum GET {
     Devices,
     Homes,
     HomeDevices{homeid: String},
-    Users{homeid: String},
+    HomeUsers{homeid: String},
     Invites{invitetoken: String},
     Users,
 }
@@ -19,7 +19,7 @@ impl API for GET {
             Self::Devices => String::from("/1/devices"),
             Self::Homes => String::from("/1/homes"),
             Self::HomeDevices { homeid } => format!("/1/homes/{}/devices", homeid),
-            Self::Users{ homeid } => format!("/1/homes/{}/users", homeid),
+            Self::HomeUsers{ homeid } => format!("/1/homes/{}/users", homeid),
             Self::Invites{ invitetoken } => format!("/1/invites/{}", invitetoken),
             Self::Users => String::from("/1/users/me"),
         }
