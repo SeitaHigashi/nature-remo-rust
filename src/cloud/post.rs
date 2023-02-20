027d0efab2c51e4617873cd76256851c3948b321
SeitaHigashi <higashi110902@gmail.com>
2023-02-21 00:29:36 +0900

    feat: add a method for API trait

--- src/cloud/post.rs
+++ src/cloud/post.rs
@@ -10,7 +10,7 @@ use home::Home;
 use signal::Signal;
 
 pub enum POST {
-    AppliancesOrders,
+    ApplianceOrders,
     Appliances{applianceid: Option<String>, appliance: Option<Appliance>},
     DetectAppliance,
     Devices{deviceid: String, device: Option<Device>},
@@ -22,6 +22,15 @@ pub enum POST {
 
 impl API for POST {
     fn path(&self) -> String {
-        todo!()
+        match self {
+            POST::ApplianceOrders => String::from("/1/appliance_orders"),
+            POST::Appliances { applianceid, appliance } => String::from("/1/appliances"),
+            POST::DetectAppliance => String::from("/1/detectappliance"),
+            POST::Devices { deviceid, device } => String::from("/1/devices"),
+            POST::Homes { homeid, home } => String::from("/1/homes"),
+            POST::Invites { invitetoken } => String::from("/1/invites"),
+            POST::Signals { signalid, signal } => String::from("/1/signals"),
+            POST::Users => String::from("/1/users"),
+        }
     }
 }
